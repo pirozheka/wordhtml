a391715cb8d53f1c7552e2ffdcdb0479db4c1c5c
Jay <you@example.com>
2024-12-18 19:11:49 +0500

    Converter is ready

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 const Header = () => {
 
     return (
-        <header className="fixed top-0 w-full h-10">
-            <div className="bg-darkblue mx-auto">
+        <header className="bg-darkblue fixed top-0 w-full h-22 z-50">
+            <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-full">
                 <div className="h-full w-full flex p-4 justify-between items-center">
                     <BsFillFileEarmarkWordFill
                         size={60}
                         color="var(--accent)"
                     />
-                    <div className="flex justify-between items-center gap-8">
-                        <Link href={'/'}>
-                            HTML Converter
+                    <div className="flex justify-between items-center gap-8 ">
+                        <Link href={'/'} className="hover:text-accent">
+                            HTML Конвертер
                         </Link>
-                        <Link href={'/translit'}>
-                            Translit
+                        <Link href={'/translit'} className="hover:text-accent">
+                            Транслитерация
                         </Link>
                     </div>
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,80 @@
-import Image from "next/image";
-
+'use client';
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+const Editor = dynamic(() => import('@tinymce/tinymce-react').then(mod => mod.Editor), {
+  ssr: false,
+});
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [editorContent, setEditorContent] = useState('');
+  const [isReady, setIsReady] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useEffect(() => {
+    setIsReady(true); // Убедитесь, что TinyMCE загружается только на клиенте.
+  }, []);
+
+  const handleCopy = () => {
+    if (navigator?.clipboard?.writeText) {
+      navigator.clipboard.writeText(editorContent).then(() => {
+        alert('Контент скопирован в буфер обмена');
+      }).catch((error) => {
+        console.error('Копирование не удалось: ', error);
+      });
+    } else {
+      // Устаревший метод
+      const textarea = document.createElement('textarea');
+      textarea.value = editorContent;
+      document.body.appendChild(textarea);
+      textarea.select();
+      try {
+        document.execCommand('copy');
+        alert('Контент скопирован в буфер обмена');
+      } catch (error) {
+        console.error('Копирование не удалось: ', error);
+      }
+      document.body.removeChild(textarea);
+    }
+  };
+
+  if (!isReady) {
+    return <p>Loading editor...</p>;
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-items-center mt-24 p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full max-w-7xl mx-auto sm:px-6 lg:px-8 h-full">
+        <Editor
+          apiKey='yi3sx9msdxosib6dph792cnf45px219mts569e5dfayqmle9'
+          init={{
+            language: 'ru',
+            plugins: [
+              // Core editing features
+              'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+
+            ],
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+          }}
+          initialValue="Введите текст сюда"
+          onEditorChange={(content) => setEditorContent(content)}
+        />
+        <div className="output w-full">
+          <div className="flex items-center justify-between w-full mb-4">
+          <h2>Перевод в HTML:</h2>
+          <button
+            className="mt-4 px-4 py-2 bg-accent text-white rounded-md hover:bg-lightblue"
+            onClick={handleCopy}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Скопировать в буфер
+          </button>
+          </div>
+          
+          <textarea
+            className="w-full min-h-28 p-2 border border-gray-300 rounded-md text-black"
+            value={editorContent}
+            readOnly
+          />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
